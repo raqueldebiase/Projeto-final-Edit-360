e416349db2966338fc8df28b1d5dce9a69656071
raqueldebiase <raqueldebiase@gmail.com>
2022-02-04 21:58:32 +0000

    Add: novo rep.

---
+++ main.js
@@ -0,0 +1,47 @@
+/*
+$("#aumentarDiv").hover (
+    function (){
+        $(this).find("a").addClass("divDestaque");
+    }, function (){
+        $(this).find("a").removeClass("divDestaque");
+    }
+);
+
+$(".eventos").hover (
+    function (){
+        $(this).find("a").addClass("divDestaque");
+    }, function (){
+        $(this).find("a").removeClass("divDestaque");
+    }
+);
+
+$(".standUp").hover (
+    function (){
+        $(this).find("a").addClass("divDestaque");
+    }, function (){
+        $(this).find("a").removeClass("divDestaque");
+    }
+);
+
+$(".workshops").hover (
+    function (){
+        $(this).find("a").addClass("divDestaque");
+    }, function (){
+        $(this).find("a").removeClass("divDestaque");
+    }
+);
+*/
+
+
+
+function aumenta(obj){
+    obj.height=obj.height*1.1;
+    obj.width=obj.width*1.1;
+}
+
+function diminui(obj){
+    obj.height=obj.height/1.1;
+    obj.width=obj.width/1.1;
+}
+
+
