e5b6256d4e5b32be69a4585f2d50e33220bcc84a
raqueldebiase <raqueldebiase@gmail.com>
2022-02-06 22:22:01 +0000

    Add: js lineUp

--- main.js
+++ main.js
@@ -1,40 +1,19 @@
-/*
-$("#aumentarDiv").hover (
-    function (){
-        $(this).find("a").addClass("divDestaque");
-    }, function (){
-        $(this).find("a").removeClass("divDestaque");
+/* Mostrar DIV dia 10*/
+
+$(".diaClicavel").each(function(){
+  $(this).on('click', function(){
+    const dia10 = $("#idDia10");
+    if (dia10.hasClass('dia10')){
+      $('#idDia10').removeClass('dia10');
+    } else {
+      $('#idDia10').addClass('dia10');
     }
-);
-
-$(".eventos").hover (
-    function (){
-        $(this).find("a").addClass("divDestaque");
-    }, function (){
-        $(this).find("a").removeClass("divDestaque");
-    }
-);
-
-$(".standUp").hover (
-    function (){
-        $(this).find("a").addClass("divDestaque");
-    }, function (){
-        $(this).find("a").removeClass("divDestaque");
-    }
-);
-
-$(".workshops").hover (
-    function (){
-        $(this).find("a").addClass("divDestaque");
-    }, function (){
-        $(this).find("a").removeClass("divDestaque");
-    }
-);
-*/
+    event.preventDefault();
+  });
+});
 
 
-
-
+/* Aumentar, diminuir span*/
 function aumenta(obj){
     obj.height=obj.height*1.1;
     obj.width=obj.width*1.1;
